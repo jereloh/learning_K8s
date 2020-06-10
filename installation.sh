df4358dcaedc54f6f49f3a688733d27d0343745f
jereloh <61404796+jereloh@users.noreply.github.com>
2020-06-10 13:18:45 +0800

    Create installation.sh
    First installation for K8s

---
+++ installation.sh
@@ -0,0 +1,8 @@
+apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+swapoff -a
+apt-get update
+apt-get install -y docker.io kubectl kubelet kubernetes-cni kubeadm
